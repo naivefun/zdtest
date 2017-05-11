af9f1e725669fb1e625acd596dea63bdf57cb962
PeiSong <naivefun@users.noreply.github.com>
2017-05-11 22:29:05 +0800

    Update question-1.js

--- question-1.js
+++ question-1.js
@@ -1,5 +1,6 @@
 module.exports.run = function replaceRepeatedChars(str) {
     if (typeof str === 'string') {
-        return str.replace(/([a-zA-Z])\1{1,}/g, '$1');
+        // return str.replace(/([a-zA-Z])\1{1,}/g, '$1');
+        return str.replace(/([a-zA-Z])\1{1,}/g, (match, contents) => { return contents + match.length; });
     }
 }
